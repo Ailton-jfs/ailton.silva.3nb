ccac9506f333bbb0126919f20710d4ee13bf9e26
Ailton-jfs <ailtonjose13@gmail.com>
2025-02-11 00:55:29 +0000

    Aulalegal

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ailton.silva.3nb
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
---
+++ Unidade-1/Aula-1-Introducao/aula1/target/debug/aula1.d
@@ -0,0 +1,1 @@
+/workspaces/ailton.silva.3nb/Unidade-1/Aula-1-Introducao/aula1/target/debug/aula1: /workspaces/ailton.silva.3nb/Unidade-1/Aula-1-Introducao/aula1/src/main.rs
